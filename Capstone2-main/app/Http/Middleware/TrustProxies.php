46b5115b28d6970919dda0f28ebf54b148e05156
AesiraGabrielle <2100916@lnu.edu.ph>
2025-09-13 21:33:56 +0800

    Encrypted

--- Capstone2-main/app/Http/Middleware/TrustProxies.php
+++ Capstone2-main/app/Http/Middleware/TrustProxies.php
@@ -10,20 +10,14 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array<int, string>|string|null
+     * @var array|string|null
      */
-protected $proxies = '*'; // trust all proxies
+    protected $proxies = '*'; // trust all proxies
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = \Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_ALL |
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB;
-        
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
