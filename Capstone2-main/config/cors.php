f77bada0ce6c196d5a2e52a8f038b8194066dfd0
AesiraGabrielle <2100916@lnu.edu.ph>
2025-09-19 17:35:45 +0800

    update test changes

--- Capstone2-main/config/cors.php
+++ Capstone2-main/config/cors.php
@@ -20,8 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://lnuwastemonitoring.onrender.com',
-        'https://frontend-testing-euvj.onrender.com',
+        '*',
 ],
     'allowed_origins_patterns' => [],
 
